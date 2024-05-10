aa1e9ad03b700d28a6c19528ebd10afb0179df83
Fernando Ricardo <138256092+fernandorrdc@users.noreply.github.com>
2024-05-09 21:01:43 -0300

    criacao api
    teste de api get

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
+/* importações */
 require('dotenv').config()
+
+const express = require('express')
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const jwt =require('jsonwebtoken')
+
+const app = express()
+
+// Rotas publicas
+app.get('/', (req, res) => {
+    res.status(200).json({ msg:  'BEM VINDO A API'})
+
+})
+
+APP.LISTEN(3000)
