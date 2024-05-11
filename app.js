3b224069ae787e83f9829dda0b9a038c749d2b54
Fernando Ricardo <138256092+fernandorrdc@users.noreply.github.com>
2024-05-11 14:14:31 -0300

    CONEXAO BD
    TESTE DE CONEXAO

--- app.js
+++ app.js
@@ -14,4 +14,15 @@ app.get('/', (req, res) => {
 
 })
 
-app.listen(3000)
+//Credenciais
+const dbUser = process.env.DB_USER
+const dbPassword = process.env.DB_PASS
+
+mongoose
+    .connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.c6q0kv2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
+    .then(() => {
+    app.listen(3000)
+    console.log('Conecção com sucesso ao BD')
+})
+.catch((err) => console.log(err)) 
+
