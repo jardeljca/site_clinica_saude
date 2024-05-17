dfc6520103e78fc8465e4bdc43860b87f93f2ca8
Fernando Ricardo <138256092+fernandorrdc@users.noreply.github.com>
2024-05-16 21:06:40 -0300

    alteracao jwt
    falta espaço

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
-const jwt =require('jsonwebtoken')
+const jwt = require('jsonwebtoken')
 
 const app = express()
 
