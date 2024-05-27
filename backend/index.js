0faa8faf43bb5ad6d8db3c29df6e5d44e86f08f3
Fernando Ricardo <fernandorrdc@hotmail.com>
2024-05-27 19:48:40 -0300

    User criacçao
    Usercontroll e UserRoutes

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express =require('express')
 const cors = require('cors')
+const UserRoutes = require('./routes/UserRoutes')
 
 const app =express()
 
@@ -14,5 +15,8 @@ app.use(cors({credentials: true, origin: 'http://localhost:3000'}))
 app.use(express.static('public'))
 
 //Rotas
+
+app.use('/users', UserRoutes)
+
 app.listen(5000)
 
