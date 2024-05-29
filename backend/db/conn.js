4fa57752bf8a7d875f604c91ac954e2b537f9f22
Fernando Ricardo <fernandorrdc@hotmail.com>
2024-05-29 18:57:18 -0300

    altercoes

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -6,12 +6,17 @@ const dbUser = process.env.DB_USER
 const dbPassword = process.env.DB_PASS
 
 async function main() {
-    
-    await mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@clusterclinicasaude.7i4e5yq.mongodb.net/?retryWrites=true&w=majority&appName=ClusterClinicaSaude`)
-    //mongodb+srv://fernandorrdc:Wsd700@clusterclinicasaude.7i4e5yq.mongodb.net/?retryWrites=true&w=majority&appName=ClusterClinicaSaude
-    
+
+    mongoose
+        .connect(
+            `mongodb+srv://${dbUser}:${dbPassword}@clusterclinicasaude.7i4e5yq.mongodb.net/?retryWrites=true&w=majority&appName=ClusterClinicaSaude`
+        )
+        //mongodb+srv://fernandorrdc:Wsd700@clusterclinicasaude.7i4e5yq.mongodb.net/?retryWrites=true&w=majority&appName=ClusterClinicaSaude
+        .then(() =>{
+            app.listen(5000)
     console.log('Conectado ao DB!')
-    
+ } )
+
 }
 main().catch((err) => console.log(err))
 module.exports = mongoose
