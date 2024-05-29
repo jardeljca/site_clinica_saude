3d49732bf6468604d4ec61b707d99318364eeacd
Jardel Queiroz <jardeljcadm@gmail.com>
2024-05-29 03:27:49 -0300

    correção do código da pasta Routers.js, correção código e chamada para metodo e login, na referida pasta tem os detalhes da correção

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,5 +1,5 @@
 import mongoose, { model } from '../db/conn';
-const { Schema } = mongoose;
+const { Schema } = mongoose; // correçao da importação do Schema com S maiúsculo
 
 const UserSchema = new Schema(
     {
@@ -22,3 +22,36 @@ const UserSchema = new Schema(
 const User = model('User', UserSchema);
 
 export default User;  // Aqui está corrigido
+
+
+
+
+/* Como estava o código antes da correção:
+
+const { Timestamp } = require("mongodb");
+const mongoose = require('../db/conn');
+const { schema } = mongoose;
+
+const User = mongoose.model(
+    'User',
+    new schema(
+        {
+            name: {
+                type: String,
+                required: true,
+            },
+            sobre: {
+                type: String,
+                required: false,
+            },
+            senha: {
+                type: String,
+                required: true,
+            },
+        },
+        { Timestamps: true },
+    ),
+);
+
+const exports = User;
+*/
--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -1,6 +1,15 @@
-const router = require('express').Router()
+const router = require('express').Router();
+const UserController = require('../controllers/UserController');
+
+router.post('/register', UserController.register); // Rota de registro
+router.post('/login', UserController.login);       // Rota de login
+
+module.exports = router;
+
+
+/* const router = require('express').Router()
 const UserController = require('../controllers/UserController')
 
 router.post('/register',UserController.register)
 
-module.exports = router
+module.exports = router */
